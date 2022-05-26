d0dced9c04bcc16070928e502b4ef4e62c0b606f
guillaume.larue <guillaume.larue@epita.fr>
2022-05-26 11:19:34 +0200

    Add role en user role assignment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import random
 
 import discord
 from discord.ext import commands
@@ -26,7 +27,6 @@ async def members(ctx: discord.context):
         cat_name = await bot.wait_for('message', timeout=15.0)
     except asyncio.TimeoutError:
         await ctx.channel.send('You ran out of time to answer!')
-    # await print(reply_message)
 
     await ctx.channel.send(f'Your name of team is **{cat_name.content}**')
 
@@ -42,12 +42,27 @@ async def members(ctx: discord.context):
                 f"{select2string(select)} are your teammates. A category and a channel will be create")
 
     await ctx.send("Choose your teammate(s)", view=Choose())
-    await ctx.guild.create_category(cat_name.content)
-    # TODO : create some channels
-    # TODO : create the role
-    # await ctx.guild.create_role(name="role name")
-    # TODO : Add persons on the role
+
+    # TODO : create private category and private channels
+    cat = await ctx.guild.create_category(cat_name.content)
+    # await guild.create_text_channel('annonces', category=cat)
+    # await guild.create_text_channel('liens-importants', category=cat)
+    # await guild.create_text_channel('general', category=cat)
+    # await guild.create_text_channel('annonces', category=cat)
+    # await guild.create_voice_channel('audio', category=cat)
+
+    # TODO : create the role with permissions
+    color = random.randint(0, 0xffffff)
+    role = await ctx.guild.create_role(name=str(cat_name.content), colour=discord.Colour(color))
+    # 0b11111111111111111111111111111111111111111
+
+    # Add persons on the role
+    for usr in users:
+        usr_tmp = ctx.guild.get_member(usr[2])
+        await usr_tmp.add_roles(role)
+
     # TODO : refacto the code in classes
+    # TODO : add try catch everywhere
     # TODO : Docker to be run everywhere
     return users
 
